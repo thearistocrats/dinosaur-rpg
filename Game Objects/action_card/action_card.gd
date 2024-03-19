9eeeed27bf2eaddc3babb9f66b492a8b927f5316
thearistocrats <60242922+thearistocrats@users.noreply.github.com>
2024-03-18 21:55:32 -0400

    Create action_card.gd

---
+++ Game Objects/action_card/action_card.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+var card_dimensions = Vector2(150, 200)
+
+const dinosaur_script = preload("res://scripts/Dinosaur Scripts/Dinosaur.gd")
+const action_script = dinosaur_script.action_script
+var DamageTypes = action_script.DamageTypes
+var ActionTypes = action_script.ActionTypes
+
+signal selected(index: int)
+var index = -1
+
+func _ready():
+	$CollisionShape2D.shape.size = card_dimensions
+
+func set_card(action, new_index, action_type):
+	index = new_index
+	match action.action_type:
+		ActionTypes.ATTACK:
+			set_attack_card_text(action)
+		ActionTypes.HEAL:
+			set_heal_card_text(action)
+
+func set_attack_card_text(attack:action_script.Attack):
+	$Name.text = attack.name
+	$Information.text = "DMG: "+str(attack.damage)+"\nST: "+str(attack.stamina_cost)+"\n"+DamageTypes.keys()[attack.damage_type]
+
+func set_heal_card_text(heal:action_script.Heal):
+	$Name.text = heal.name
+	$Information.text = "HP: "+str(heal.heal)+"\nST: "+str(heal.stamina_cost)+("\nOverheal"if heal.is_over_heal else "\n ")
+	pass
+
+func _on_selected_card(_viewport, event, _shape_idx):
+	if index < 0: pass
+	if event is InputEventMouseButton && event.pressed:
+		selected.emit(index)
+		#print(index)
+
